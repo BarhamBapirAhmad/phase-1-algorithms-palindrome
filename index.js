b5aba5d796d375ea1349afd215c20357af579b01
BarhamBapirAhmad <barhamabpirahmed@gmail.com>
2024-03-12 17:52:03 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function isPalindrome(word) {
+for (let i = 0; i < word.length; i++) {
+  if (word[i] !== word[word.length - 1 - i]) {
+    return false;
+  }}
+
+  return true;
+  
+
+
+
   // Write your algorithm here
 }
 
